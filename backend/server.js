f4f37c4ffb6a2425601652c31ccfad67f70420e2
Erfan Khan <erfan.khan.cse@gmail.com>
2025-08-04 13:13:17 +0600

    Nodemon and Setting up Routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.send("you got 5 notes");
+  res.status(200).send("you got 55 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Note created succesfully" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note updated succesfully" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Note deleted succesfully" });
 });
 
 app.listen(5001, () => {
